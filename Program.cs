45e6c22a6013e3d8a77d2fe6bcf84def32c41414
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-10-23 10:08:50 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_davide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
